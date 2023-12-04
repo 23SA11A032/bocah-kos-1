9a069d6f1a6b978252d26d736cf1bae0657e32c8
AKHMAD FAUZAN <ozan6825@gmail.com>
2023-12-04 12:36:56 +0000

    y

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -72,6 +72,26 @@ export default function Home() {
                                 </div>
                             )
                         })}
+
+                        {Array.from({ length: 10 }).map((_, i) => {
+                            return (
+                                <div className="card card-compact bg-base-100 shadow-xl border border-slate-700" key={i}>
+                                    <figure><img src="https://www.sewakost.com/files/01-2020/ad28904/kos-kosan-443380741_large.jpg" alt="Shoes" /></figure>
+                                    <div className="card-body">
+                                        <div className="flex flex-row items-center gap-2">
+                                            <div className="badge badge-secondary">PUTRI</div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                                                <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
+                                            </svg>
+                                            <p>5.0</p>
+                                        </div>                                        
+                                        <p className='truncate'>Kost Singgahsini RR Mansion Tipe E Kost Singgahsini RR Mansion Tipe E</p>
+                                        <p className='text-sm italic'>k. mandi dalam, wifi, kasur.</p>
+                                        <p>Rp. 900.000</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
