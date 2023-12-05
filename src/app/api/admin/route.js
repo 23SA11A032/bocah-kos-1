1220f6734c15b8bf445759f098f844413d30c53e
23SA11A032 <152951901+23SA11A032@users.noreply.github.com>
2023-12-05 15:49:16 +0000

    y

--- src/app/api/admin/route.js
+++ src/app/api/admin/route.js
@@ -1,6 +1,6 @@
 import connection from "@/databases/connection"
 
 export async function GET() {
-    var [query] = await connection.promise().execute("SELECT * FROM test")
+    var [query] = await connection.promise().execute("SELECT * FROM bocahkost")
     return Response.json(query)
 }
